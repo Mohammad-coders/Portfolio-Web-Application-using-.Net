3b213fe6995fbfb5b4bb0a290d73794472b64bf1
Mohammad-coders <arahaman461@gmail.com>
2024-01-28 23:47:51 +0600

    done Education.cshtml

--- Portfolio/Controllers/HomeController.cs
+++ Portfolio/Controllers/HomeController.cs
@@ -26,5 +26,11 @@ namespace Portfolio.Controllers
 
             return View();
         }
+        public ActionResult Education()
+        {
+            ViewBag.Message = "Your Education page.";
+
+            return View();
+        }
     }
 }
